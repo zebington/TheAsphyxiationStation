12a0c68d6a5b5d5672ce0e91d29b343d5c00dc58
A.Aurangzeb U1560821 <U1560821@unimail.hud.ac.uk>
2018-10-24 14:52:10 +0100

    Player and Ladder Scripts

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public float speed = 6;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    float speed = 3.0f;
+    [SerializeField]
+    float oxygenTank = 100; // players health
+
+    Rigidbody2D rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+
+        LoseOxygenOverTime();
+    }
+
+    void LoseOxygenOverTime()
+    {
+        oxygenTank -= 1 * Time.deltaTime;
+    }
+}
