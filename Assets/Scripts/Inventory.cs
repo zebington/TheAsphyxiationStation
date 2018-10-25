6664443d29c7ee5dc8df042996731512eb52dd42
Aqib Aurangzeb <aqibaurangzebdeveloper@gmail.com>
2018-10-25 02:22:22 +0100

    Inventory System added, items can be picked up easily

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public bool[] isFull;
+
+    public GameObject[] slots;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject itemButton;
+
+	// Use this for initialization
+	void Start () {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            for(int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    // item can be added
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            } 
+        }
+    }
+}
