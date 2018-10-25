0b0410f91e13b135ce6a7c7872288ac816d7e6e5
Aqib Aurangzeb <aqibaurangzebdeveloper@gmail.com>
2018-10-25 01:08:20 +0100

    AI mechanic, chase player when within range

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public Transform[] patrolPoints;
+    public float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+    public Transform target;
+    public float chaseRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+        if(distanceToTarget < chaseRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime); // from, to, speed
+
+        }
+
+
+	}
+}
